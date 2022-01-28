c2c5e337504430fdfbf72ad28f915eb8556f9eb8
veysel mutlu <veysel_mutlu42@hotmail.com>
2022-01-28 22:51:34 +0300

    Application dependency startup.cs classes in added and configured

---
+++ src/Services/Auctions/OnlineAuctionApp.AuctionAPI/Enums/EStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineAuctionApp.AuctionAPI.Enums
+{
+    public enum EStatus : byte
+    {
+        Active = 0,
+        Closed = 1,
+        Passive = 2
+    }
+}
---
+++ src/Services/Auctions/OnlineAuctionApp.AuctionAPI/Startup.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using OnlineAuctionApp.AuctionAPI.DataAccess.Abstract;
+using OnlineAuctionApp.AuctionAPI.DataAccess.Concrete;
+using OnlineAuctionApp.AuctionAPI.Settings.Abstract;
+using OnlineAuctionApp.AuctionAPI.Settings.Concrete;
+
+namespace OnlineAuctionApp.AuctionAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<SourcingDatabaseSettings>(Configuration.GetSection("SourcingDatabaseSetting"));
+
+            services.AddSingleton<ISourcingDatabaseSettings>(s => s.GetRequiredService<IOptions<SourcingDatabaseSettings>>().Value);
+
+            services.AddScoped<IAuctionContext, AuctionContext>();
+
+            services.AddScoped<IAuctionRepository, AuctionRepository>();
+
+            services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "OnlineAuctionApp.AuctionAPI", Version = "v1" });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "OnlineAuctionApp.AuctionAPI v1"));
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
