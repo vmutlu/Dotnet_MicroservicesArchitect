80f59cb43ad004b5c6a4a86041aff4340574a8a6
veysel mutlu <veysel_mutlu42@hotmail.com>
2022-01-28 22:50:10 +0300

    Poco classes created(Teklif ve ihale sınıflarıı)

---
+++ src/Services/Auctions/OnlineAuctionApp.AuctionAPI/Entities/Concrete/Auction.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using OnlineAuctionApp.AuctionAPI.Enums;
+using System;
+using System.Collections.Generic;
+
+namespace OnlineAuctionApp.AuctionAPI.Entities.Concrete
+{
+    public class Auction
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ProductId { get; set; }
+        public int Quantity { get; set; }
+        public DateTime StartedAt { get; set; }
+        public DateTime FinishedDate { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public EStatus Status { get; set; }
+        public List<string> Sellers { get; set; }
+    }
+}
---
+++ src/Services/Auctions/OnlineAuctionApp.AuctionAPI/Entities/Concrete/Bid.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace OnlineAuctionApp.AuctionAPI.Entities.Concrete
+{
+    public class Bid
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string AuctionId { get; set; }
+        public string ProductId { get; set; }
+        public string SellerUserName { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public decimal Price { get; set; }
+    }
+}
