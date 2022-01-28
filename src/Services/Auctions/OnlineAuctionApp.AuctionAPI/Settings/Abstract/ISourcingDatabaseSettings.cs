33598b1ac248d4d73641b3dfa57c1a0d4d7db125
veysel mutlu <veysel_mutlu42@hotmail.com>
2022-01-28 22:50:51 +0300

    Auction project database settings process completed

---
+++ src/Services/Auctions/OnlineAuctionApp.AuctionAPI/Settings/Abstract/ISourcingDatabaseSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace OnlineAuctionApp.AuctionAPI.Settings.Abstract
+{
+    public interface ISourcingDatabaseSettings
+    {
+        public string ConnectionStrings { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
---
+++ src/Services/Auctions/OnlineAuctionApp.AuctionAPI/Settings/Concrete/SourcingDatabaseSettings.cs
@@ -0,0 +1,10 @@
+﻿using OnlineAuctionApp.AuctionAPI.Settings.Abstract;
+
+namespace OnlineAuctionApp.AuctionAPI.Settings.Concrete
+{
+    public class SourcingDatabaseSettings : ISourcingDatabaseSettings
+    {
+        public string ConnectionStrings { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
