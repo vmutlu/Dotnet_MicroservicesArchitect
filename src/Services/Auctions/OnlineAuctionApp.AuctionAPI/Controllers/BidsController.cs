96b7ce6fe342dd58ce8dbc5e8e6f3cd201f4b8df
veysel mutlu <veysel_mutlu42@hotmail.com>
2022-01-28 22:51:06 +0300

    Empty endpoints added

---
+++ src/Services/Auctions/OnlineAuctionApp.AuctionAPI/Controllers/BidsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineAuctionApp.AuctionAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class BidsController : ControllerBase
+    {
+    }
+}
