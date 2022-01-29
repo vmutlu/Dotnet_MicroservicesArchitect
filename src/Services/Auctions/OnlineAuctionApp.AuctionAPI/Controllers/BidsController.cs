f45ed7396d35df124e06c366e796f031719ecedf
veysel mutlu <veysel_mutlu42@hotmail.com>
2022-01-29 14:17:22 +0300

    BidController added and endpoints writed

--- src/Services/Auctions/OnlineAuctionApp.AuctionAPI/Controllers/BidsController.cs
+++ src/Services/Auctions/OnlineAuctionApp.AuctionAPI/Controllers/BidsController.cs
@@ -1,11 +1,58 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OnlineAuctionApp.AuctionAPI.DataAccess.Abstract;
+using OnlineAuctionApp.AuctionAPI.Entities.Concrete;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
 
-namespace OnlineAuctionApp.AuctionAPI.Controllers
+namespace OnlineBidApp.BidAPI.Controllers
 {
     [Route("api/v1/[controller]")]
     [ApiController]
     public class BidsController : ControllerBase
     {
+        private readonly IBidRepository _repository;
+        private readonly ILogger<BidsController> _logger;
+
+        public BidsController(IBidRepository repository, ILogger<BidsController> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        [HttpGet("{id:length(24)}")]
+        [ProducesResponseType(typeof(IEnumerable<Bid>), statusCode: (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Bid>>> GetBidByAuctionId(string id)
+        {
+            var bids = await _repository.GetBindsByAuctionId(id).ConfigureAwait(false);
+
+            return Ok(bids);
+        }
+
+        [HttpGet("{id:length(24)}")]
+        [ProducesResponseType(statusCode: (int)HttpStatusCode.NotFound)]
+        [ProducesResponseType(typeof(Bid), statusCode: (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Bid>> GetWinnerBid(string id)
+        {
+            var bid = await _repository.GetWinnerBid(id).ConfigureAwait(false);
+
+            if (bid is null)
+            {
+                _logger.LogError($"Bid with Id: {id}, hasn't been found in database");
+                return NotFound(bid);
+            }
+
+            return Ok(bid);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(statusCode: (int)HttpStatusCode.Created)]
+        public async Task<ActionResult> SendBid([FromBody] Bid bid)
+        {
+            await _repository.SendBind(bid).ConfigureAwait(false);
+
+            return Ok();
+        }      
     }
 }
