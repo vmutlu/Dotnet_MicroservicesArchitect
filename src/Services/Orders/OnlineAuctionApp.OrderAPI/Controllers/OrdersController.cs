09340f2e445126d57c898e416342804797e95edb
veysel mutlu <veysel_mutlu42@hotmail.com>
2022-02-08 19:57:27 +0300

    OrdersController created

---
+++ src/Services/Orders/OnlineAuctionApp.OrderAPI/Controllers/OrdersController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineAuctionApp.OrderAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+    }
+}
