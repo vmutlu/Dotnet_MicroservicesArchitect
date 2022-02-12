347351d5521103f01eaca4de96798b577fd32daa
veysel mutlu <veysel_mutlu42@hotmail.com>
2022-02-12 13:26:28 +0300

    command classes writed

---
+++ src/Services/Orders/OnlineAuctionApp.Application/Commands/OrderCreate/OrderCreateValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace OnlineAuctionApp.Application.Commands.OrderCreate
+{
+    public class OrderCreateValidator : AbstractValidator<OrderCreateCommand>
+    {
+        public OrderCreateValidator()
+        {
+            RuleFor(o => o.SellerUserName).EmailAddress().NotEmpty();
+            RuleFor(o => o.ProductId).NotEmpty();
+        }
+    }
+}
