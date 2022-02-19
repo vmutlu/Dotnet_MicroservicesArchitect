6d676f822ea00bd94b7bb3ccd6a3e942643309e0
veysel mutlu <veysel_mutlu42@hotmail.com>
2022-02-19 20:13:27 +0300

    Order classes prop added

--- src/Services/Orders/OnlineAuctionApp.Domain/Entities/Concrete/Order.cs
+++ src/Services/Orders/OnlineAuctionApp.Domain/Entities/Concrete/Order.cs
@@ -6,6 +6,7 @@ namespace OnlineAuctionApp.Domain.Entities.Concrete
     public class Order: Entity
     {
         public string AuctionId { get; set; }
+        public string Description { get; set; }
         public string SellerUserName { get; set; }
         public string ProductId { get; set; }
         public decimal UnitPrice { get; set; }
--- src/Services/Orders/OnlineAuctionApp.Infrastructure/Migrations/20220219165057_Order_Table_Updated.Designer.cs
+++ src/Services/Orders/OnlineAuctionApp.Infrastructure/Migrations/20220219165057_Order_Table_Updated.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OnlineAuctionApp.Infrastructure.Concrete.Context;
 
 namespace OnlineAuctionApp.Infrastructure.Migrations
 {
     [DbContext(typeof(OrderContext))]
-    partial class OrderContextModelSnapshot : ModelSnapshot
+    [Migration("20220219165057_Order_Table_Updated")]
+    partial class Order_Table_Updated
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
