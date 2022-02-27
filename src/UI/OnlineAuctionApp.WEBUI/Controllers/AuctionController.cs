3d7130c2a6802bd9762625a24c92ffeddf7e71db
veysel mutlu <veysel_mutlu42@hotmail.com>
2022-02-27 11:05:35 +0300

    Auction(teklif) page created

---
+++ src/UI/OnlineAuctionApp.WEBUI/Controllers/AuctionController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace OnlineAuctionApp.WEBUI.Controllers
+{
+    public class AuctionController : Controller
+    {
+        private readonly ILogger<AuctionController> _logger;
+        public AuctionController(ILogger<AuctionController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/UI/OnlineAuctionApp.WEBUI/Models/AuctionModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineAuctionApp.WEBUI.Models
+{
+    public class AuctionModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please fill Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please fill Description")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Please fill ProductId")]
+        public string ProductId { get; set; }
+
+        [Required(ErrorMessage = "Please fill Quantity")]
+        public int Quantity { get; set; }
+
+        [Required(ErrorMessage = "Please fill StartedDate")]
+        public DateTime StartedDate { get; set; }
+
+        [Required(ErrorMessage = "Please fill FinishedDate")]
+        public DateTime FinishedDate { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int Status { get; set; }
+        public int SellerId { get; set; }
+        public List<string> Sellers { get; set; }
+    }
+}
