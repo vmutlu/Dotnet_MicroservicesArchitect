904527909970fc5ddf130acbd33e77dc4fdb5e41
veysel mutlu <veysel_mutlu42@hotmail.com>
2022-03-03 19:39:11 +0300

    Aop imptlementation it was continued

---
+++ src/UI/OnlineAuctionApp.Core/Commons/CommonInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineAuctionApp.Core.Commons
+{
+    public static class CommonInfo
+    {
+        public static string BaseAddress = "http://localhost:5000";
+        public static string AuctionBaseAddress = "http://localhost:8001";
+        public static string ProductBaseAddress = "http://localhost:8000";
+    }
+}
---
+++ src/UI/OnlineAuctionApp.Core/Commons/ResultConstant.cs
@@ -0,0 +1,18 @@
+﻿namespace OnlineAuctionApp.Core.Commons
+{
+    public static class ResultConstant
+    {
+        public const string RecordFound = "Record Found";
+        public const string RecordNotFound = "Record Not Found";
+        public const string RecordCreateSuccessfully = "Record Create Successfully";
+        public const string RecordCreateNotSuccessfully = "Record Create Not Successfully";
+        public const string RecordRemoveNotSuccessfully = "Record Remove Not Successfully";
+        public const string RecordRemoveSuccessfully = "Record Remove Successfully";
+
+        public const string Admin_Email = "veysel_mutlu42@hotmail.com";
+        public const string Admin_Password = "Password2021.";
+
+        public const string LoginUserInfo = "Login User Info";
+        public const string MailTagHelperSuffix = "github.com/vmutlu";
+    }
+}
